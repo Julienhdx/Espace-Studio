90abb2315a1e1c1a78a9a9c26d280f393a1c9658
Julienhdx <jujuhendrickx@gmail.com>
2025-11-05 05:39:11 +0100

    Add files via upload

---
+++ send_form.php
@@ -0,0 +1,51 @@
+<?php
+
+error_reporting(0); 
+
+
+$to_email = "jujufaitlapomme123@mail.com"; 
+
+$redirect_page = "inscription.html"; 
+
+
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['form_type'])) {
+    
+    $form_type = $_POST['form_type'];
+    $subject = "Nouveau Questionnaire Shooting Photo - " . ($form_type === 'particulier' ? "Particulier" : "Entreprise");
+    $message = "Type de Formulaire : " . ($form_type === 'particulier' ? "Particulier" : "Entreprise") . "\n\n";
+
+
+    // ⚠️ IMPORTANT : REMPLACEZ VOTRE_DOMAINE PAR VOTRE NOM DE DOMAINE RÉEL
+    $headers = "From: webmaster@VOTRE_DOMAINE\r\n"; 
+    $headers .= "Reply-To: noreply@VOTRE_DOMAINE\r\n"; 
+    $headers .= "X-Mailer: PHP/" . phpversion();
+
+
+    foreach ($_POST as $key => $value) {
+        if ($key !== 'form_type') {
+
+            $clean_key = str_replace("_", " ", $key); 
+            $clean_key = ucwords($clean_key);       
+            
+            $message .= $clean_key . " : " . $value . "\n";
+        }
+    }
+
+
+
+    if (mail($to_email, $subject, $message, $headers)) { 
+
+        header("Location: " . $redirect_page . "?status=success");
+        exit;
+    } else {
+ 
+        header("Location: " . $redirect_page . "?status=error");
+        exit;
+    }
+
+} else {
+
+    header("Location: " . $redirect_page);
+    exit;
+}
+?>
